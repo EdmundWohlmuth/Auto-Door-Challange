0977bb29320a8e606a036fafa16c219c15472edb
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-08 08:48:39 -0400

    Created door mesh

---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // when object has entered the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // when object leaves trigger
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}

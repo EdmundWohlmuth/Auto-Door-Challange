cc26ce035269254e6a49cac45313f5e6f95ae48a
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-08 09:20:44 -0400

    basic open/close

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
+    public GameObject door;
+    bool open;
+    float movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = 2.5f;
     }
 
     // when object has entered the trigger
     private void OnTriggerEnter(Collider other)
     {
-        
+        door.transform.position =
+            new Vector3(door.transform.position.x, door.transform.position.y + movement, door.transform.position.z);
+        open = false;
     }
 
     // when object leaves trigger
     private void OnTriggerExit(Collider other)
     {
-        
+        door.transform.position =
+            new Vector3(door.transform.position.x, door.transform.position.y - movement, door.transform.position.z);
+        open = true;
     }
+
 
 }
